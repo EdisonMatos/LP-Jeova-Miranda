27596ac78f9a109e008c4b6b1d75fb086aed29ec
Gabriel de Souza <154693365+BiellSouza@users.noreply.github.com>
2025-03-04 15:06:05 +0000

    fix: Ajusta bug do fundo ao esticar formulário

--- src/components/interactives/WhatsappLinks.jsx
+++ src/components/interactives/WhatsappLinks.jsx
@@ -11,7 +11,7 @@ export default function WhatsAppLinks() {
       //   backgroundRepeat: "",
       // }}
     >
-      <div className="flex items-center justify-center w-full h-screen">
+      <div className="flex items-center justify-center w-full">
         <div className="flex flex-col items-center p-4">
           <MotionDivDownToUp className="flex justify-center desktop1:justify-start">
             <img
